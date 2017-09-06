4d2d0aa2ffccdaa3c27981109293be42d7556c2f
liwei <l443986991@qq.com>
2017-09-06 19:04:32 +0800

    更新代码到轮播

--- js/index.js
+++ js/index.js
@@ -1,16 +1,42 @@
 $(function () {
-    var url = 'http://192.168.1.102:9900/api';
+    var url = 'http://192.168.70.63:9900/api';
 
     //主导航
     $.ajax({
-        url: 'http://192.168.1.102:9900/api/nav',
+        url: 'http://192.168.70.63:9900/api/nav',
         datatype: 'json',
         success: function (data) {
             var $data = JSON.parse(data);
-            // console.log($data);
             var res = template('mainNav', $data);
-            // console.log(res)
             $('.zdh').html(res);
+
+            $('.zdh li').hover(function () {
+                var type = $(this).attr('data-type');
+                if (type) {
+                    $.ajax({
+                        url: url + '/nav',
+                        data: {
+                            type: type
+                        },
+                        datatype: 'json',
+                        success: function (data) {
+                            var $data = JSON.parse(data);
+                            var res = template('mainNavList', {
+                                data: $data
+                            });
+                            $('.mainNavList ul').html(res);
+                        }
+                    })
+                }
+                $('.mainNavList').stop().slideDown(500);
+            }, function () {
+                $('.mainNavList').stop().slideUp(500);
+            })
+            $('.mainNavList').hover(function () {
+                $('.mainNavList').stop().slideDown(500);
+            }, function () {
+                $('.mainNavList').stop().slideUp(500);
+            })
         }
     })
 
@@ -28,6 +54,59 @@ $(function () {
         $('.seachList').slideUp();
     });
 
+    //轮播图
+    $.ajax({
+        url: url + '/lunbo',
+        success: function (data) {
+            var $data = JSON.parse(data);
+            // console.log($data);
+            var res = template('slide', $data);
+            $('.slide ul').html(res);
+
+            var index = 0;
+            var len = $('.slide li').length;
+            $('.btn-right').click(function () {
+                auto();
+            })
+            $('.btn-left').click(function () {
+                index--;
+                if (index < 0) {
+                    index = len - 1;
+                }
+                $('.slide li').fadeOut().eq(index).fadeIn();
+            })
+
+            function auto() {
+
+                index++;
+                if (index > len - 1) {
+                    index = 0;
+                }
+                $('.slide li').fadeOut().eq(index).fadeIn();
+
+            }
+            var timerId = setInterval(auto, 2000);
+            $('.slide').mouseover(function () {
+                clearInterval(timerId);
+            })
+            $('.slide').mouseout(function () {
+                timerId = setInterval(auto, 2000)
+            })
+
+        }
+    })
+
+    //轮播左侧导航
+    $.ajax({
+        url: url + '/items',
+        datatype: JSON,
+        success: function (data) {
+            // console.log(data)
+            var $data = JSON.parse(data);
+            var res = template('menu', $data);
+            $('.menu ul').html(res);
+        }
+    })
 
 
 })
